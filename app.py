10ffcc7c8a0515e00ac7aece6a00b9e2a855e797
JAYADIXITHA S P <jayadixitha.vidyanidhi@gmail.com>
2025-04-21 19:38:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "<h1>Hello from Render Flask App!</h1>"
+    return render_template('index.html')  # Ensure this file is in the templates folder
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
